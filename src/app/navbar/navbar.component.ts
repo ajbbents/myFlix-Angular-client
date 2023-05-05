24404b8bce0d9cdbf7844da52d46ec655010c2fd
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-05-05 11:39:14 -0500

    navbar styling and construction

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  constructor(public router: Router) { }
+  //navigates to movies view
+  toMovies(): void {
+    this.router.navigate(['movies']);
+  }
 
+  //navigates to profile view
+  toProfile(): void {
+    this.router.navigate(['profile']);
+  }
+
+  //logs out user
+  logout(): void {
+    this.router.navigate(['welcome']);
+    localStorage.clear();
+  }
 }

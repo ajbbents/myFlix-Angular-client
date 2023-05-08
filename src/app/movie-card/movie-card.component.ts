f653f6862f1cf07009894dc51719f6f0a848fe03
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-05-08 11:03:19 -0500

    hook up genre, synopsis, director dialogs

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 // This import brings in the API calls we created in 6.2
 import { UserRegistrationService } from '../fetch-api-data.service';
-// import { DirectorComponent } from '../director/director.component';
+import { DirectorComponent } from '../director/director.component';
+import { GenreComponent } from '../genre/genre.component';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -10,6 +12,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
+
+//movie card component fetches and displays all movies
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
   favorites: any[] = [];
@@ -38,10 +42,12 @@ export class MovieCardComponent implements OnInit {
     });
   }
 
+  //checks to see if movie is a favorite
   isFavorite(id: string): boolean {
     return this.favorites.includes(id);
   }
 
+  //adds movie to favorites
   addToFavorites(id: string): void {
     this.fetchApiData.addFavoriteMovie(id).subscribe((result) => {
       this.snackBar.open('movie added to favorites', 'okay', {
@@ -51,6 +57,7 @@ export class MovieCardComponent implements OnInit {
     });
   }
 
+  //removes movie from favorites
   removeFromFavorites(id: string): void {
     this.fetchApiData.removeFavoriteMovie(id).subscribe((result) => {
       this.snackBar.open('movie removed from favorites', 'okay', {
@@ -60,5 +67,38 @@ export class MovieCardComponent implements OnInit {
     });
   }
 
+  //open movie synopsis dialog
+  openSynopsis(title: string, description: string): void {
+    this.dialog.open(MovieDetailsComponent, {
+      data: {
+        Title: title,
+        Description: description,
+      },
+      width: '25rem',
+    });
+  }
+
+  //open movie genre dialog
+  openGenre(name: string, description: string): void {
+    this.dialog.open(GenreComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+      width: '25rem',
+    });
+  }
+
+  //open movie director dialog
+  openDirector(name: string, bio: string, birthday: string): void {
+    this.dialog.open(DirectorComponent, {
+      data: {
+        Name: name,
+        Bio: bio,
+        Birth: birthday,
+      },
+      width: '25rem',
+    });
+  }
 
 }
